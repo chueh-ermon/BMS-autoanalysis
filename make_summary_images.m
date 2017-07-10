30599313a081ae78215d0a60d45201491c83162d
petermattia <pattia@stanford.edu>
2017-07-10 11:50:37 -0700

    make summary images update

--- make_summary_images.m
+++ make_summary_images.m
@@ -5,23 +5,45 @@ function make_summary_images(batch, batchname)
 
 %% Q vs n
 
+xlabel('Cycle number')
+ylabel('Remaining discharge capacity (Ah)')
+saveas('summary2_Q')
 
 %% Make plots for each
 % Batch 1 (2017-05-12)
 if batchname == 'batch1'
-    %% Capacity vs time
+    %% Capacity vs charging time
     
-    %% Average degradation vs time
     
     xlabel('Time to 80% SOC (minutes)')
-    ylabel('Degradation rate')
+    ylabel('Remaining discharge capacity (Ah)')
+    saveas(gcf, 'summary2_Q_vs_t80.png')
+    
+    %% Average degradation vs charging time
+    
+    xlabel('Time to 80% SOC (minutes)')
+    ylabel('Average degradation rate (Ah/cycle)')
+    saveas(gcf, 'summary3_degrate_vs_t80.png')
+    
+    
+    %% Contour plot
+    
+    saveas(gcf, 'summary4_contour.png')
     
 % Batch 2 (2017-06-30)
 elseif batchname == 'batch2'
     %% Contour plots
+    % x = CC1, y = CC2, contours = Q1
+    
+    saveas(gcf, 'summary2_contour1.png')
+    
+    
+    % x = CC1, y = Q1, contours = CC2
+    
+    saveas(gcf, 'summary3_contour2.png')
     
 else
-    warning('Batch name not recognized')
+    warning('Batch name not recognized. No summary figures generated')
 end
 
 end
