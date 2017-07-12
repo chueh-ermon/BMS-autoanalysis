92a508f8aa7754f31fc8a24002045ffca070e17b
mchen18 <mhchen96@gmail.com>
2017-07-12 12:32:57 -0700

    fixing plotting for batch1

--- batch1_summary_plots.m
+++ batch1_summary_plots.m
@@ -24,8 +24,8 @@ function batch1_summary_plots(batch, batch_name, T_cells, T_policies)
     
     % Find Capacities
     for i = num_cells
-        cycle = max(cycle_num(i)) % pulls the end cycle
-        capacities(i) = batteries(i).cycles(cycle).Qd % pulls the most recent capacity
+        cycle = cycle_num(i);
+        capacities(i) = batch(i).summary.QDischarge(cycle); % pulls the most recent capacity
     end
     
     %% Capacity vs charging time
@@ -51,49 +51,52 @@ function batch1_summary_plots(batch, batch_name, T_cells, T_policies)
     print('summary4_deg_vs_t80','-dpng')
     
     %% Contour plot
-    % adapted from Peter Attia
-    % extract from policies
+    % adapted from Peter Attia    
     
-    CC1 = T_policies(:,1); % CC1
-    Q1 = T_policies(:,2); % Q1
-    CC2 = T_policies(:,3); % CC2 
-    degradation_rate = T_policies(:,6); % Deg rate
-    
-    time = ones(length(CC2),length(CC1),length(Q1));
+    % extract data for scatter plot
+    CC1 = table2array(T_policies(:,1)); % CC1
+    Q1 = table2aray(T_policies(:,2)); % Q1
+    CC2 = table2array(T_policies(:,3)); % CC2 
+    degradation_rate = table2array(T_policies(:,8)); % Deg rate
+        
+    % initialize for contour lines
     figure, set(gcf, 'units','normalized','outerposition',[0 0 1 1])
+    Q1_array=5:5:80;
+    CC1_array=3:0.5:10;
+    CC2_array=[3 3.6];
+    time = ones(length(CC2_array),length(CC1_array),length(Q1_array));
+
     for i=1:length(CC2)
         subplot(1,length(CC2),i), hold on, box on
         xlabel('CC1'),ylabel('Q1 (%)')
         title(['CC2 = ' num2str(CC2(i))])
         %axis([min(CC1) max(CC1) min(Q1) max(Q1)])
         
-        [X,Y] = meshgrid(CC1,Q1);
-        time = (60.*Y./X./100)+(60.*(80-Y)./CC2(i)./100);
+        [X,Y] = meshgrid(CC1_array,Q1_array);
+        time = (60.*Y./X./100)+(60.*(80-Y)./CC2_array(i)./100);
         v = [13, 12, 11, 10, 9, 8];
         contour(X,Y,time,v,'LineWidth',2,'ShowText','on')
     end
-    
-    % call to T_cells and T_policies
-    
+        
     % prepare for degradation plotting
     colormap jet;
     scalefactor = 1e6; % Factor to scale
-    maxvalue = max(list(:,4))*scalefactor;
+    maxvalue = max(degradation_rate*scalefactor);
 
-    for i = 1:length(list(:,1))
-        if list(i,3) == 3
+    for i = 1:length(CC1)
+        if CC2(i) == 3
             subplot(1, length(CC2), 1)
             %ax(2) = axes;
-            scatter(list(i,1),list(i,2),'o','CData',list(i,4)*scalefactor,'SizeData',200,'LineWidth',5)
-        elseif list(i,3) == 3.6
+            scatter(CC1(i),Q1(i),'o','CData',degradation_rate(i)*scalefactor,'SizeData',200,'LineWidth',5)
+        elseif CC2(i) == 3.6
             subplot(1, length(CC2), 2)
-            scatter(list(i,1),list(i,2),'o','CData',list(i,4)*scalefactor,'SizeData',200,'LineWidth',5)
+            scatter(CC1(i),Q1(i),'o','CData',degradation_rate(i)*scalefactor,'SizeData',200,'LineWidth',5)
         else
             subplot(1, length(CC2), 1)
-            scatter(list(i,1),list(i,2),'square','CData',list(i,4)*scalefactor,'SizeData',200,'LineWidth',5)
+            scatter(CC1(i),Q1(i),'square','CData',degradation_rate(i)*scalefactor,'SizeData',200,'LineWidth',5)
             caxis([0 maxvalue])
             subplot(1, length(CC2), 2)
-            scatter(list(i,1),list(i,2),'square','CData',list(i,4)*scalefactor,'SizeData',200,'LineWidth',5)
+            scatter(CC1(i),Q1(i),'square','CData',degradation_rate(i)*scalefactor,'SizeData',200,'LineWidth',5)
             caxis([0 maxvalue])
         end
     end
