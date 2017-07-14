69aa683275bfc7ae4229b75a5c1bcb8137bb2f79
chueh-battery <chueh-ermon@users.noreply.github.com>
2017-07-13 20:04:03 -0700

    Fixed contour plots

--- batch2_summary_plots.m
+++ batch2_summary_plots.m
@@ -7,23 +7,23 @@ function batch2_summary_plots(batch, batch_name, T_cells, T_policies)
 
 %% Initialization and inputs
 T_policies = table2array(T_policies); % convert to array
-T_cells = table2array(T_cells); % convert to arrayT
+% T_cells = table2array(T_cells); % convert to array (not really needed)
 T_size = size(T_policies);
-time = 10; % target time of policies
+time = 10; % target time of policies, in minutes
 colormap jet;
-scalefactor = 1e6; % factor to scale degradation rates by
+scalefactor = 5e5; % factor to scale degradation rates by
 maxvalue = max(T_policies(:,8))*scalefactor; % scale degradation rate
 
 
 %% Initialize plot 1
-figure % x = CC1, y = Q1, contours = CC2
+contour1 = figure; % x = CC1, y = Q1, contours = CC2
 set(gcf, 'units','normalized','outerposition',[0 0 1 1]) % resize for screen
 set(gcf,'color','w') % make figures white
-hold on
+hold on, box on
 Q1=0.5:0.1:79.5;
 CC1=1:0.02:6;
 [X,Y] = meshgrid(CC1,Q1);
-CC2 = [(time - (Y./100).*(60./X))./(60.*(0.8-(Y./100)))].^(-1);
+CC2 = ((time - (Y./100).*(60./X))./(60.*(0.8-(Y./100)))).^(-1);
 CC2_values = T_policies(:,3); % creates vector of CC2 values
 
 % plot contour values for plot 1
@@ -31,50 +31,52 @@ contour(X,Y,CC2,CC2_values,'LineWidth',2,'ShowText','on')
 % scatter plot policies with performance data
 for i = 1:T_size(1)
     if T_policies(i,2) == 80
-            figure(1)
+            figure(contour1)
             scatter(T_policies(i,1),T_policies(i,2),'rsquare','CData',T_policies(i,8)*scalefactor,'SizeData',250,'LineWidth',5)
     else
-            figure(1)
+            figure(contour1)
             scatter(T_policies(i,1),T_policies(i,2),'ro','CData',T_policies(i,8)*scalefactor,'SizeData',250,'LineWidth',5)
     end
     caxis([0 maxvalue])
-
 end
 xlabel('CC1'),ylabel('Q1 (%)')
 
-%% Save file
-saveas(gcf, 'summary3_contour1.png')
+% Cover bad region
+line([1.02 4.75],[16.5 79], 'LineWidth',20,'color','w');
 
-        
+%% Save file
+saveas(contour1, 'summary3_contour1.png')
+
 %% Initialize plot 2
-figure(2) % x = CC1, y = CC2 contours = Q1
+contour2 = figure; % x = CC1, y = CC2 contours = Q1
 set(gcf, 'units','normalized','outerposition',[0 0 1 1]) % resize for screen
 set(gcf,'color','w') % make figures white
-hold on
-CC1=1:0.02:6;
-CC2 = 3:0.1:6;
+hold on, box on, axis square
+CC1 = 1:0.1:8;
+CC2 = 1:0.1:8;
 [X,Y] = meshgrid(CC1,CC2);
-Q1 = (100).*[(time - ((60*0.8)./Y))./((60./X)-(60./Y))];
+Q1 = (100).*(time - ((60*0.8)./Y))./((60./X)-(60./Y));
 Q1_values = 5:10:75;
 
-% plot contour values for plot 1 
+% plot contour values for plot 2
 contour(X,Y,Q1,Q1_values,'LineWidth',2,'ShowText','on')
 for i = 1:T_size(1)
     if T_policies(i,2) == 80
-            figure(2)
+            figure(contour2)
             scatter(T_policies(i,1),T_policies(i,3),'rsquare','CData',T_policies(i,8)*scalefactor,'SizeData',250,'LineWidth',5)
     else
-            figure(2)
+            figure(contour2)
             scatter(T_policies(i,1),T_policies(i,3),'ro','CData',T_policies(i,8)*scalefactor,'SizeData',250,'LineWidth',5)
     end
     caxis([0 maxvalue])
 end
 xlabel('CC1'),ylabel('CC2')
 
-% legend loop
-
+%% Cover bad region
+line([1 4.7],[1 4.7], 'LineWidth',20,'color','w')
+line([4.9 8],[4.9 8], 'LineWidth',20,'color','w');
 
 %% Save file
-saveas(gcf, 'summary4_contour2.png')
+saveas(contour2, 'summary4_contour2.png')
 
 end
