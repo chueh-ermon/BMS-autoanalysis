0b2b492c53546cf6982fc713cd65991a01482dcb
petermattia <pattia@stanford.edu>
2017-07-21 00:38:26 -0700

    make_summary_gifs

--- make_summary_gifs.m
+++ make_summary_gifs.m
@@ -1,4 +1,4 @@
-function make_summary_gifs(batch, batch_name, T_cells, T_policies)
+function make_summary_gifs(batch, batch_name)
 % make_summary_gifs makes the summary gifs for each batch. Since each
 % batch will have different 'best' ways of presenting data, we use
 % conditional statements to identify which to use
@@ -10,21 +10,10 @@ filename = ['Qnplot_' batch_name '.gif']; % Specify the output file name
 endcycle = 1000; % Last cycle plotted
 
 %% Move to GIF directory
-cd 'C:\Users\Arbin\Box Sync\Data\Batch GIFS\'
-
-%% Preinitialize random colors and markers
-n_cells = height(T_cells);
-n_policies = height(T_policies);
-
-cols = cell(1,n_policies);
-marks = cell(1,n_policies);
-for i = 1:n_policies
-    [col, mark]=random_color('y','y');
-    cols{i} = col;
-    marks{i} = mark;
-end
+%cd 'C:\Users\Arbin\Box Sync\Data\Batch GIFS\'
 
 %% Find all unique policies
+n_cells = numel(batch);
 policies = cell(n_cells,1);
 readable_policies = cell(n_cells,1);
 for i = 1:n_cells
@@ -33,16 +22,38 @@ for i = 1:n_cells
 end
 unique_policies = unique(policies);
 unique_readable_policies = unique(readable_policies);
+n_policies = numel(unique_policies);
+
+%% Preinitialize random colors and markers
+cols = cell(1,n_policies);
+marks = cell(1,n_policies);
+for i = 1:n_policies
+    [col, mark]=random_color('y','y');
+    cols{i} = col;
+    marks{i} = mark;
+end
 
 %% Create 'Q' array - cell array of cell arrays
 % Q = 1xn cell, where n = number of policies
 % Q{1,1} = 1xm cell, where m = number of cells tested for this policy
-Q = cell(1,n_policies); 
+Q = cell(1,n_policies);
 for i = 1:n_policies % loop through all policies
-    for j = 1:n_cells
+    numcells = 0;
+    for j = 1:n_cells % cell index
         if strcmp(unique_policies{i}, batch(j).policy)
-            Q{i} = {Q{i} batch(j).summary.QDischarge};
+            numcells = numcells + 1;
         end
+    end
+    Q{i} = cell(1,numcells);
+    
+    j = 1; % cell index
+    k = 1; % cell = policy index
+    while k < numcells + 1
+        if strcmp(unique_policies{i}, batch(j).policy)
+            Q{i}(k) = {batch(j).summary.QDischarge};
+            k = k + 1;
+        end
+        j = j + 1;
     end
 end
 
@@ -50,10 +61,9 @@ end
 figure('units','normalized','outerposition',[0 0 1 1]), box on
 xlabel('Cycle number')
 ylabel('Remaining discharge capacity (Ah)')
-axis([0 1000 0.85 1.15])
+axis([0 1000 0.80 1.2])
 set(gcf, 'Color' ,'w')
 hline(0.88)
-leg = columnlegend(2,unique_readable_policies,'Location','NortheastOutside','boxoff');
 
 %% Begin looping. j = cycle index
 for j=1:endcycle
@@ -64,7 +74,6 @@ for j=1:endcycle
         
         cycles = j.*ones(1,length(Q{i}));
         Qn = zeros(1,length(Q{i})); % preinitialize capacity at cycle n (Qn)
-        Qend = zeros(1,length(Q{i})); % preinitialize capacity at cycle n (Qn)
         % k = index of cells within a policy
         for k = 1:length(Q{i})
             % If cell has died, we won't have data at this cycle number.
@@ -74,15 +83,15 @@ for j=1:endcycle
             else
                 Qn(k) = Q{i}{k}(j);
             end
-            
-            Qend(k) = Q{i}{k}(end);
         end
         
         % Plot points for this policy
-        scatter(cycles,Qn2,100,cols{i},marks{i},'LineWidth',1.5);
+        scatter(cycles,Qn,100,cols{i},marks{i},'LineWidth',1.5);
     end
     % Misc plotting stuff
     title(['Cycle ' num2str(j)])
+    %leg = columnlegend(2,unique_readable_policies,'Location','NortheastOutside','boxoff');
+    leg = legend(unique_readable_policies','Location','EastOutside');
     hold off
     
     % Create GIF
@@ -97,26 +106,6 @@ for j=1:endcycle
     end
 end
 
-
-figAbsolute = figure('units','normalized','outerposition',[0 0 1 1]); hold on, box on
-for i = 1:length(unique_policies)
-    % Keep consistent color
-    [col, mark] = random_color('y','y');
-    %All the markers we want to use
-    markers = {'+','o','*','.','x','s','d','^','v','>','<','p','h'};
-    % Find all cells with policy i, generate combined x,y
-    x=double.empty;
-    y=double.empty;
-    for j = 1:numel(batch)
-        if strcmp(unique_policies{i}, batch(j).policy)
-            x = cat(2,x,batch(j).summary.cycle);
-            y = cat(2,y,batch(j).summary.QDischarge);
-        end
-    end
-    figure(figAbsolute);
-    plot(x,y,markers{mod(j,numel(markers))+1},'color',col);
-end
-
 % %% Make different summary plots for each batch
 % % Batch 1 (2017-05-12)
 % if batch_name == 'batch1'
@@ -129,7 +118,7 @@ end
 % end
 
 close all
-cd 'C:/Users/Arbin/Documents/GitHub/BMS-autoanalysis'
+%cd 'C:/Users/Arbin/Documents/GitHub/BMS-autoanalysis'
 
 disp('Completed make_summary_gifs'),toc
 
